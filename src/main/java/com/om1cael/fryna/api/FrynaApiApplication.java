576eea6e419d112a90cd7fe5e8e29efddce7f54e
om1cael <188113439+om1cael@users.noreply.github.com>
2025-09-28 11:48:18 -0300

    ever ever ever

---
+++ src/main/java/com/om1cael/fryna/api/FrynaApiApplication.java
@@ -0,0 +1,13 @@
+package com.om1cael.fryna.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrynaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrynaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/om1cael/fryna/api/FrynaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.om1cael.fryna.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrynaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
